dbbc381e10686e706444a7a6218e636228439de6
Jouini-Mohamed-Chaker <JouiniMohamedChaker@proton.me>
2024-12-03 08:49:37 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 mkdir -p out
 javac -d out src/lox/*.java
-java -cp out lox.Lox
+if [ $? -ne 0 ]; then
+  echo "Compilation failed!"
+  exit 1
+fi
+echo "Compilation successful"
+
+if [ $# -eq 1 ]; then
+  java -cp out lox.Lox "$1"
+else
+  java -cp out lox.Lox
+fi
